0cf113ab3bd591c70945e46050754c786c0b6dc5
1031900805@qq.com <1031900805@qq.com>
2019-06-30 11:31:19 +0800

    添加了initData/View、layoutId

--- common_base/src/main/java/com/example/common_base/BaseActivity.kt
+++ common_base/src/main/java/com/example/common_base/BaseActivity.kt
@@ -1,5 +1,32 @@
 package com.example.common_base
 
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
 /**
- * Description:
+ * Description:所有activity得父类抽取了一些公用方法
  */
+abstract class BaseActivity : AppCompatActivity() {
+
+    abstract fun getLayoutId():Int
+
+    protected abstract fun initView()
+    protected abstract fun initData()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(getLayoutId())
+
+        setStatusBarColor()
+
+        initView()
+        initData()
+    }
+
+    /**
+     * 初始化statusBar颜色
+     */
+    private fun setStatusBarColor(){
+
+    }
+}
