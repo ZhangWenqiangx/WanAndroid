116bd04e1714451a5b71451ab821cabdf5d9b33f
1031900805@qq.com <1031900805@qq.com>
2019-07-01 20:57:24 +0800

    Arouter在kotlin中有不同的集成方式， 以优化

---
+++ common_base/src/main/java/com/example/common_base/mvp/BasePresenter.kt
@@ -0,0 +1,6 @@
+package com.example.common_base.mvp
+
+/**
+ * Description:
+ */
+class BasePresenter
--- module_usercenter/src/main/java/com/example/module_usercenter/activity/LoginActivity.kt
+++ module_usercenter/src/main/java/com/example/module_usercenter/activity/LoginActivity.kt
@@ -2,8 +2,11 @@ package com.example.module_usercenter.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.alibaba.android.arouter.facade.annotation.Route
+import com.example.common_base.AConstance
 import com.example.module_usercenter.R
 
+@Route(path = AConstance.ACTIVITY_URL_LOGIN)
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
