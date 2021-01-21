67b0e0859dfaec383e698759efcbfbe2d66bcc34
zhangwenqiang <zhang.wenqiang@eflagcomm.com>
2021-01-21 17:59:15 +0800

    添加了网络请求耗时日志

--- lib_common/src/main/java/com/example/common_base/http/RetrofitClient.java
+++ lib_common/src/main/java/com/example/common_base/http/RetrofitClient.java
@@ -5,28 +5,10 @@ import com.franmontiel.persistentcookiejar.PersistentCookieJar;
 import com.franmontiel.persistentcookiejar.cache.SetCookieCache;
 import com.franmontiel.persistentcookiejar.persistence.SharedPrefsCookiePersistor;
 
-import java.net.Socket;
-import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.Principal;
-import java.security.PrivateKey;
-import java.security.SecureRandom;
-import java.security.cert.CertificateException;
-import java.security.cert.X509Certificate;
 import java.util.concurrent.TimeUnit;
 
-import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSession;
-import javax.net.ssl.SSLSocketFactory;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.TrustManagerFactory;
-import javax.net.ssl.X509KeyManager;
-import javax.net.ssl.X509TrustManager;
-
 import okhttp3.OkHttpClient;
+import okhttp3.logging.LoggingEventListener;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -47,10 +29,10 @@ public class RetrofitClient {
                 .connectTimeout(15, TimeUnit.SECONDS)
                 .readTimeout(15, TimeUnit.SECONDS)
                 .cookieJar(cookieJar)
+                .eventListenerFactory(new LoggingEventListener.Factory())
                 .addInterceptor(InterceptorUtil.headerInterceptor())
                 .addInterceptor(InterceptorUtil.logInterceptor())
                 .build();
-
         retrofit = new Retrofit.Builder()
                 .baseUrl(API_HOST)
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
