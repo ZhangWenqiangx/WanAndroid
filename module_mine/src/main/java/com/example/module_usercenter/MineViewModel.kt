6a8c520bacf5743f88a244f859e01ed6bc7f6e52
zhangwenqiang <zhang.wenqiang@eflagcomm.com>
2021-09-08 16:21:53 +0800

    个人页面配置

--- module_mine/src/main/java/com/example/module_usercenter/MineViewModel.kt
+++ module_mine/src/main/java/com/example/module_usercenter/MineViewModel.kt
@@ -4,8 +4,10 @@ import androidx.lifecycle.MutableLiveData
 import com.example.common_base.base.data.BaseResult
 import com.example.common_base.base.viewmodel.BaseViewModel
 import com.example.common_base.base.viewmodel.ErrorState
+import com.example.common_base.constants.FlutterConstance
 import com.example.common_base.util.UserHelper
 import com.example.module_usercenter.api.MineRepository
+import com.example.module_usercenter.bean.MenuBean
 import com.example.module_usercenter.bean.UserInfoResult
 
 /**
@@ -35,4 +37,44 @@ class MineViewModel(
         })
     }
 
+    fun generateMenuList(): MutableList<MenuBean> {
+        return mutableListOf<MenuBean>().apply {
+            add(
+                MenuBean(
+                    imageResId = R.drawable.ic_coin,
+                    title = "我的积分",
+                    routeUrl = FlutterConstance.FLUTTER_PAGE_COIN_LIST,
+                )
+            )
+            add(
+                MenuBean(
+                    imageResId = R.drawable.ic_rank,
+                    title = "积分排行",
+                    routeUrl = FlutterConstance.FLUTTER_PAGE_COIN_RANK,
+                )
+            )
+            add(
+                MenuBean(
+                    imageResId = R.drawable.ic_share,
+                    title = "我的分享",
+                    routeUrl = FlutterConstance.FLUTTER_PAGE_SHARE,
+                )
+            )
+            add(
+                MenuBean(
+                    imageResId = R.drawable.ic_collect,
+                    title = "我的收藏",
+                    routeUrl = FlutterConstance.FLUTTER_PAGE_COLLECTION,
+                )
+            )
+            add(
+                MenuBean(
+                    imageResId = R.drawable.ic_mine_setting,
+                    title = "系统设置",
+                    routeUrl = FlutterConstance.FLUTTER_PAGE_COIN_LIST,
+                )
+            )
+        }
+
+    }
 }
